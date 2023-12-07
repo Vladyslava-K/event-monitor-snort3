41c7c2c9db1554f97ca58b23a8e76a96b3c04f26
vladislava <vlada.kryazh@gmail.com>
2023-12-07 18:15:32 +0200

    add RequestLog model

--- snort3_monitor/request/models.py
+++ snort3_monitor/request/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class RequestLog(models.Model):
+    """
+    Model representing user requests log.
+
+    Attributes:
+    - timestamp (datetime): Request time
+    - user_ip (str): users IP address
+    - http_method (str): HTTP request method
+    - request_data (json): contains query parameters and endpoint
+    """
+    timestamp = models.DateTimeField()
+    user_ip = models.CharField(max_length=30)
+    http_method = models.CharField()
+    request_data = models.JSONField()
