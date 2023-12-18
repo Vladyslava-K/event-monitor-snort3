50832180656650aa8e8ddebea7b07af372a848ff
vladislava <vlada.kryazh@gmail.com>
2023-12-18 18:35:50 +0200

    added dockerfile

--- snort3_monitor/snort3_monitor/settings.py
+++ snort3_monitor/snort3_monitor/settings.py
@@ -138,6 +138,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:63342",
     "http://localhost:3000",
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
 ]
 
 try:
